dbb6e44f0ce0472ce110a17e6eb43bb489d1a4eb
samwhelp <samwhelp@users.noreply.github.com>
2026-09-14 20:33:06 +0800

    fix

--- remote-install.sh
+++ remote-install.sh
@@ -2,4 +2,45 @@
 
 
 
-echo 'remote-install.sh'
+
+##
+## ## Update apt repository information
+##
+
+sudo apt-get update
+
+
+
+
+##
+## ## Install Packages / calamares
+##
+
+sudo apt-get -y install calamares calamares-settings-debian
+
+
+
+
+##
+## ## Install Packages / git
+##
+
+sudo apt-get -y git
+
+
+
+
+##
+## ## Clone Project
+##
+
+git clone -b anduinos-2.0.2 https://github.com/samwhelp/calamares-settings-debian-adjustment-for-anduinos.git calamares-settings
+
+
+
+
+##
+## ## Install My Settings
+##
+
+sudo cp -rfT ./calamares-settings/asset/overlay /
